2a58e62beaa40a9fad2a07265447eaed2cc89b37
vuongc4298 <156062555+vuongc4298@users.noreply.github.com>
2026-03-17 22:50:49 +0700

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', "https://vuongc4298-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"]
-CSRF_TRUSTED_ORIGINS = ["https://vuongc4298-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"]
+ALLOWED_HOSTS = ['localhost','https://harshshinde8-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://harshshinde8-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -62,9 +62,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/static'),
-            os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),
+           os.path.join(BASE_DIR,'frontend/static'),
+           os.path.join(BASE_DIR, 'frontend/build'),
+           os.path.join(BASE_DIR, 'frontend/build/static'), 
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -139,8 +139,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR,'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
